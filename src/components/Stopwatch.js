344396c124b788a780d6b59a6f81d8cc1c4b4487
Mike790 <michael.kydd@outlook.com>
2024-02-23 14:46:41 -0500

    improve stopwatch logic

--- src/components/Stopwatch.js
+++ src/components/Stopwatch.js
@@ -1,24 +1,46 @@
 import React, { useEffect, useState } from 'react'
 
 function Stopwatch() {
-  const [time, setTime] = useState({hours: 0, minutes: 0, seconds:0})
+  const [time, setTime] = useState({hours: 0, minutes: 0, seconds:57})
   const [start, setStart] = useState(false)
-  
+
   useEffect(() => {
     if (!start) return
     setTimeout(() => {
-      setTime({...time, seconds: time.seconds+1})
+      if (time.minutes === 59 && time.seconds === 59) {
+        setTime({hours: time.hours + 1, minutes: 0, seconds: 0})
+        console.log(9999)
+        return
+      }
+  
+      if (time.seconds === 59) {
+        setTime({...time, minutes: time.minutes + 1, seconds: 0})
+        return
+      }
+  
+      setTime({...time, seconds: time.seconds + 1})
       console.log("Time:", time)
     }, 1000);
   }, [time, start])
+
+  function displayTime() {
+    let displayTime;
+    if (time.hours > 0) {
+      displayTime = `${String(time.hours).padStart(2, 0)}:${String(time.minutes).padStart(2, 0)}:${String(time.seconds).padStart(2, 0)}`
+      console.log('displayTime', displayTime)
+    } else if (time.minutes > 0){
+      displayTime = `${String(time.minutes).padStart(2, 0)}:${String(time.seconds).padStart(2, 0)}`
+    } else {
+      displayTime = `00:${String(time.seconds).padStart(2, 0)}`
+    }
+    return displayTime
+  }
 
   return (
     <div>
       <button onClick={() => {setStart(true)}}>Start Workout</button>
       <br />
-      {time.hours > 0 ? time.hours : null}
-      {time.minutes > 0 ? time.minutes : null}
-      {time.seconds > 0 ? time.seconds : null}
+      {displayTime()}
       <br />
       <button onClick={() => {setStart(false)}}>End Workout</button>
     </div>
