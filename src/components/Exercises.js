26da442a4f8eb2659e57c8992bc597e5e6bf9ea4
Mike790 <michael.kydd@outlook.com>
2024-03-03 16:57:31 -0500

    conditionally render chest exercises

--- src/components/Exercises.js
+++ src/components/Exercises.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 // import { useEffect, useState } from 'react';
 // import { getData } from '../util/getExercises';
 import { allExercises } from '../util/Data';
@@ -12,16 +12,33 @@ function Exercises() {
   //   })
   // }, [])
 
+  // function getExercises(bodyParts) {
+  //   for (let i=0; i<bodyParts.length; i++) {
+      
+  //   }
+  // }
+  const [showChest, setShowChest] = useState(false)
+
   return (
     <div>
-      {console.log(allExercises)}
-      <ul>
-        {allExercises.map(exercise => {
-          return <li key={exercise.id}>{exercise.name}</li>
-        })}
-      </ul>
+      <button onClick={() => setShowChest(!showChest)}>Chest</button>
+      {showChest && <ul>
+          {allExercises.filter((exercise) => exercise.bodyPart === 'chest').map((exercise) => {
+            return <li key={exercise.id}>{exercise.name}</li>
+          })}
+        </ul>}
+      <button>Back</button>
+      <button>Arms</button>
+      <button>Shoulders</button>
+      <button>Core</button>
+      <button>Legs</button>
+      {showChest && <ul>
+          {allExercises.filter((exercise) => exercise.bodyPart === 'chest').map((exercise) => {
+            return <li key={exercise.id}>{exercise.name}</li>
+          })}
+        </ul>}
     </div>
   )
 }
 
-export default Exercises
+export default Exercises;
