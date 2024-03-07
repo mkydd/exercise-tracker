e6e6c91a3cfcd93f04e3fb08f17a302a234a82be
Mike790 <michael.kydd@outlook.com>
2024-03-07 16:17:31 -0500

    display current exercises

--- src/components/CurrentExercises.js
+++ src/components/CurrentExercises.js
@@ -3,7 +3,11 @@ import React from 'react'
 function CurrentExercises({ exercises }) {
   return (
     <div>
-
+      <ul>
+        {exercises.map((exercise) => {
+          return <li key={exercise.id}>{exercise.name}</li>
+        })}
+      </ul>
     </div>
   )
 }
