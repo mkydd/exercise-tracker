da70f7ec0309aa66b6e20d171c71a0a16116efe6
Mike790 <michael.kydd@outlook.com>
2024-03-07 15:39:02 -0500

    create CurrentExercise component

---
+++ src/components/CurrentExercises.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function CurrentExercises() {
+  return (
+    <div>CurrentExercises</div>
+  )
+}
+
+export default CurrentExercises
--- src/components/Workout.js
+++ src/components/Workout.js
@@ -3,12 +3,14 @@ import WorkoutHeader from "./WorkoutHeader";
 import Stopwatch from './Stopwatch';
 import AddExerciseButton from './AddExerciseButton';
 import CompletedExercises from './CompletedWorkout';
+import CurrentExercises from './CurrentExercises';
 
 function Workout() {
   return (
     <div>
       <WorkoutHeader />
       <Stopwatch />
+      <CurrentExercises />
       <AddExerciseButton />
       <CompletedExercises />
     </div>
