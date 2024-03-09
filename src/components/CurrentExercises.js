8663596e7c45c244640aecfd2bcded9ae62c4170
Mike790 <michael.kydd@outlook.com>
2024-03-08 22:25:36 -0500

    track sets

--- src/components/CurrentExercises.js
+++ src/components/CurrentExercises.js
@@ -1,11 +1,31 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 function CurrentExercises({ exercises, removeExercise }) {
+  const [allSets, setAllSets] = useState([])
+  function addSet(exercise) {
+    allSets.forEach((exerciseSets, index) => {
+      if (exerciseSets.exerciseId === exercise.id) {
+        setAllSets([...allSets, {...exerciseSets, sets: [{setNumber: 1, reps: 0}]}])
+        return
+      }
+    })
+    setAllSets([...allSets, {exerciseId: exercise.id, sets: [{setNumber: 1, reps: 0}]}])
+  }
   return (
     <div>
       <ul>
         {exercises.map((exercise) => {
-          return <li key={exercise.id} onClick={() => removeExercise(exercise)}>{exercise.name}</li>
+          return (
+              <li key={exercise.id} >
+                {exercise.name} -
+                <button onClick={() => {addSet(exercise)}}>Add Set</button>
+                <ul>
+                  Sets
+                  {console.log('allSets =', allSets)}
+                </ul>
+                
+                {/* <button onClick={}>Add Rep</button> */}
+              </li>)
         })}
       </ul>
     </div>
