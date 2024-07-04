063728708e002cfedd042ce770c40ee70d817c00
Mike790 <michael.kydd@outlook.com>
2024-07-03 22:30:44 -0400

    define schema

--- backend/models/Workout.js
+++ backend/models/Workout.js
@@ -1,7 +1,35 @@
 const mongoose = require('mongoose');
 
 const WorkoutSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    trim: true,
+    maxlength: [50, 'name can not be more than 50 characters']
+  },
+  date: {
+    type: Date,
+    required: [true, 'must provide date']
+  },
+  location: {
+    street: {
+      type: String,
+      trim: true,
+      maxlength: [50, 'street can not be more than 50 characters']
+    },
+    city: {
+      type: String,
+      trim: true,
+      maxlength: [50, 'city can not be more than 50 characters']
+    },
+    
 
+  },
+  // duration: {
+
+  // },
+  // exercises: {
+
+  // }
 });
 
 module.exports = mongoose.model('Workout', WorkoutSchema);
