df789f5b71b3fab6dbb21ee0c6391adc7e3a4a6d
Mike790 <michael.kydd@outlook.com>
2024-07-12 14:34:37 -0400

    implement asyncWrapper

--- backend/controllers/workouts.js
+++ backend/controllers/workouts.js
@@ -1,21 +1,22 @@
 const Workout = require('../models/Workout')
+const asyncWrapper = require('../middleware/async')
 
-const getAllWorkouts = async (req, res) => {
+const getAllWorkouts = asyncWrapper(async (req, res) => {
   const workouts = await Workout.find({})
   res.status(200).json({ workouts })
-}
+})
 
-const createWorkout = async (req, res) => {
+const createWorkout = asyncWrapper(async (req, res) => {
   const workout = await Workout.create(req.body)
   res.status(201).json({ workout })
-}
+})
 
-const getWorkout = async (req, res) => {
+const getWorkout = asyncWrapper(async (req, res) => {
   const workout = await Workout.findById(req.params.id)
   res.status(200).json({ workout })
-}
+})
 
-const updateWorkout = async (req, res) => {
+const updateWorkout = asyncWrapper(async (req, res) => {
   const workout = await Workout.findOneAndUpdate(
     {_id: req.params.id},
     req.body,
@@ -23,12 +24,12 @@ const updateWorkout = async (req, res) => {
     runValidators: true })
 
   res.status(200).json({ workout })
-}
+})
 
-const deleteWorkout = async (req, res) => {
+const deleteWorkout = asyncWrapper(async (req, res) => {
   const workout = await Workout.findByIdAndDelete(req.params.id)
   res.status(200).json({ workout })
-}
+})
 
 module.exports = {
   getAllWorkouts,
