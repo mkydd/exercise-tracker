f1ab8f17a16ca328e8d9f938c48c90ae999725cc
Mike790 <michael.kydd@outlook.com>
2024-07-13 23:12:14 -0400

    create user controller

---
+++ backend/controllers/users.js
@@ -0,0 +1,30 @@
+
+
+
+const getAllUsers = (req, res) => {
+
+}
+
+const createUser = (req, res) => {
+
+}
+
+const getUser = (req, res) => {
+
+}
+
+const updateUser = (req, res) => {
+
+}
+
+const deleteUser = (req, res) => {
+
+}
+
+module.exports = {
+  getAllUsers,
+  createUser,
+  getUser,
+  updateUser,
+  deleteUser
+}
