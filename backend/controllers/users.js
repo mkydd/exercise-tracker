0b3c11b160e24a79995f72de37a10dc9c33ba052
Mike790 <michael.kydd@outlook.com>
2024-07-13 23:49:14 -0400

    implement user controller

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -1,24 +1,34 @@
+const User = require('../models/User')
 
 
-
-const getAllUsers = (req, res) => {
-
+const getAllUsers = async (req, res) => {
+  const users = await User.find({})
+  res.status(200).json({ users })
 }
 
-const createUser = (req, res) => {
-
+const createUser = async (req, res) => {
+  const user = await User.create(req.body)
+  res.status(201).json({ user })
 }
 
-const getUser = (req, res) => {
-
+const getUser = async (req, res) => {
+  const user = await User.findById(req.params.id)
+  res.status(200).json({ user })
 }
 
-const updateUser = (req, res) => {
+const updateUser = async (req, res) => {
+  const user = await User.findByIdAndUpdate(
+    {_id: req.params.id},
+    req.body,
+    { new: true,
+    runValidators: true})
 
+  res.status(200).json({ user })
 }
 
-const deleteUser = (req, res) => {
-
+const deleteUser = async (req, res) => {
+  const user = await User.findOneAndDelete(req.params.id)
+  res.status(200).json({ user })
 }
 
 module.exports = {
