37d2061ed01ca5db216ffd5cc9e8bb370261837b
Mike790 <michael.kydd@outlook.com>
2024-07-13 23:07:46 -0400

    create user routes

--- backend/app.js
+++ backend/app.js
@@ -1,16 +1,20 @@
 const express = require('express');
 const bodyParser  = require('body-parser');
 const app = express();
-const workouts = require('./routes/workouts')
+
 const connectDB = require('./db/connect')
 require('dotenv').config()
 app.use(bodyParser.json());
+
+const workouts = require('./routes/workouts')
+const users = require('./routes/users')
 const errorHandlerMiddleware = require('./middleware/error-handling')
 
 
 
 // routes
 app.use('/api/v1/users/workouts', workouts)
+app.use('api/v1/users', users)
 app.use(errorHandlerMiddleware)
 
 const port = process.env.PORT || 5001;
