a24c42553a07bc0e0312e369e6703e8d25cf0be6
Mike790 <michael.kydd@outlook.com>
2024-07-14 21:20:00 -0400

    add async handler

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -1,35 +1,49 @@
 const User = require('../models/User')
+const asyncWrapper = require('../middleware/async')
+const { createCustomError } = require('../errors/custom-error')
 
-
-const getAllUsers = async (req, res) => {
+const getAllUsers = asyncWrapper(async (req, res) => {
   const users = await User.find({})
   res.status(200).json({ users })
-}
+})
 
-const createUser = async (req, res) => {
+const createUser = asyncWrapper(async (req, res) => {
   const user = await User.create(req.body)
   res.status(201).json({ user })
-}
+})
 
-const getUser = async (req, res) => {
+const getUser = asyncWrapper(async (req, res, next) => {
   const user = await User.findById(req.params.id)
-  res.status(200).json({ user })
-}
+  if (!user) {
+    return next(createCustomError(`No user with id : ${req.params.id}`, 404), req,res)
+  }
 
-const updateUser = async (req, res) => {
+  res.status(200).json({ user })
+})
+
+const updateUser = asyncWrapper(async (req, res, next) => {
   const user = await User.findByIdAndUpdate(
     {_id: req.params.id},
     req.body,
     { new: true,
     runValidators: true})
 
-  res.status(200).json({ user })
-}
+  if (!user) {
+    return next(createCustomError(`No user with id: ${req.params.id}`, 404), req, res)
+  }  
 
-const deleteUser = async (req, res) => {
-  const user = await User.findOneAndDelete(req.params.id)
   res.status(200).json({ user })
-}
+})
+
+const deleteUser = asyncWrapper(async (req, res, next) => {
+  const user = await User.findOneAndDelete(req.params.id)
+
+  if (!user) {
+    return next(createCustomError(`No user with id: ${req.params.id}`, 404), req, res)
+  }
+
+  res.status(200).json({ user })
+})
 
 module.exports = {
   getAllUsers,
