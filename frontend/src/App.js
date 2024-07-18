6c05a6d9fb83193351afe63da52f818732a12fc6
Mike790 <michael.kydd@outlook.com>
2024-07-17 22:53:24 -0400

    implement AUTH0

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,13 @@
 import Workout from "./components/Workout/Workout";
+import LoginButton from "./components/Auth/Login";
+import LogoutButton from "./components/Auth/Logout";
 
 function App() {
   return (
     <div className="App">
       <Workout />
+      <LoginButton />
+      <LogoutButton />
     </div>
   );
 }
