c8603c44da1e697d6cacae7cd35d2d2e7121b250
Mike790 <michael.kydd@outlook.com>
2024-07-17 22:29:47 -0400

    add auth0 component

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,9 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 
+import { Auth0Provider } from '@auth0/auth0-react';
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Auth0Provider
+    domain=""
+    clientId=""
+    authorizationParams={{
+      redirect_uri: window.location.origin
+    }}
+    >
+      <App />
+    </Auth0Provider>
   </React.StrictMode>
 );
