ee02e8b75c3343bf06215b5773a4ae765f46d76c
Mike790 <michael.kydd@outlook.com>
2024-07-20 22:14:06 -0400

    remove unused routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,17 +1,12 @@
 import Workout from "./components/Workout/Workout";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
-import Home from "./components/Home";
-import PrivateRoute from "./components/Auth/PrivateRoute";
 
 function App() {
   return (
     <div className="App">
       <Router>
         <Routes>
-          <Route path="/" element={<Home />}/>
-          <Route element={<PrivateRoute />}>
-            <Route path="/workout" element={<Workout />}/>
-          </Route>
+          <Route path="/workout" element={<Workout />}/>
         </Routes>
       </Router>
     </div>
