d57fec10dfb6e53b27f996231c415e3d163e4a99
Mike790 <michael.kydd@outlook.com>
2024-07-21 00:21:57 -0400

    implement signup page

---
+++ frontend/src/pages/signUp.js
@@ -0,0 +1,51 @@
+import React, { useState } from 'react'
+
+function SignUp() {
+  const [firstName, setFirstName] = useState('')
+  const [lastName, setLastName] = useState('')
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+
+  function createUser(e) {
+    e.preventDefault()
+
+    fetch("http://localhost:5001/api/v1/users", {
+      method: 'POST',
+      body: JSON.stringify({
+        name: {
+          firstName,
+          lastName
+        },
+        email,
+        password   
+      }),
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+        'Content-Type': 'application/json'
+      }
+    })
+    .then((res) => console.log('res =', res))
+  }
+
+  return (
+    <div>
+      <form onSubmit={(e) => createUser(e)}>
+        <label htmlFor="firstName">First name:</label>
+        <input type="text" id="firstName" value={firstName} onChange={(e) => setFirstName(e.target.value)} /><br /><br />
+
+        <label htmlFor="lastName">Last name:</label>
+        <input type="text" id="lastName" value={lastName} onChange={(e) => setLastName(e.target.value)} /><br /><br />
+
+        <label htmlFor="email">Email:</label>
+        <input type="email" id="email" value={email} onChange={(e) => setEmail(e.target.value)} /><br /><br />
+
+        <label htmlFor="password">Password:</label>
+        <input type="password" id="password" value={password} onChange={(e) => setPassword(e.target.value)} /><br /><br />
+
+        <input type="submit" value="Submit" />
+      </form>
+    </div>
+  )
+}
+
+export default SignUp
