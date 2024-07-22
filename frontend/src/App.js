e4d8a27d47c06601816eceb25545d6f155149184
Mike790 <michael.kydd@outlook.com>
2024-07-21 23:13:54 -0400

    add userHome page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,7 @@ import Workout from "./components/Workout/Workout";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
 import SignUp from "./pages/signUp";
 import Login from "./pages/login";
+import UserHome from "./pages/userHome";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
           <Route path="/workout" element={<Workout />}/>
           <Route path="/signup" element={<SignUp />}/>
           <Route path="/login" element={<Login />}/>
+          <Route path="/user" element={<UserHome />}/>
         </Routes>
       </Router>
     </div>
---
+++ frontend/src/pages/userHome.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function UserHome() {
+  // useEffect(() => {
+  //   fetch(`https://localhost:5001/api/v1/users/${userId}`)
+  // }, [])
+  return (
+    <div>
+      UserHome
+      <button>Start New Workout</button>
+      <h2>Previous Workout</h2>
+
+    </div>
+  )
+}
+
+export default UserHome
