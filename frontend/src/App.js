e6aa966ee98047d722720bc3049bb6d0a6ca0ffe
Mike790 <michael.kydd@outlook.com>
2024-07-24 15:26:22 -0400

    add user routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,18 @@
 import Workout from "./components/Workout/Workout";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
 import SignUp from "./pages/signUp";
-// import Login from "./pages/login";
 import UserHome from "./pages/user/UserHome";
-import LoginButton from "./auth/login";
 import Home from "./pages/home";
+
+
+import LoginButton from "./auth/login";
 import PrivateRoute from "./auth/ProtectedRoute";
-// import { useAuth0 } from "@auth0/auth0-react"
+
+// user Routes
+import Exercises from "./pages/user/Exercises";
+import History from './pages/user/History';
+import Profile from './pages/user/Profile';
+import StartWorkout from './pages/user/StartWorkout';
 
 function App() {
   
@@ -14,18 +20,19 @@ function App() {
     <div className="App">
       <Router>
         <Routes>
-          <Route path="/" element={<Home />}/>
+          <Route path="/" element={<Home />} />
           <Route element={<PrivateRoute/>}>
-            <Route path="/user" element={<UserHome />}>
-              <Route path="/profile" element={<Home />}/>
-              <Route path="/history" element={<Home />}/>
-              <Route path="/startworkout" element={<Home />}/>
-              <Route path="/exercises" element={<Home />}/>
+            <Route path="user">
+              <Route index element={<UserHome />} />
+              <Route path="profile" element={<Profile />} />
+              <Route path="history" element={<History />} />
+              <Route path="startworkout" element={<StartWorkout />} />
+              <Route path="exercises" element={<Exercises />} />
             </Route>
           </Route>
-          <Route path="/workout" element={<Workout />}/>
-          <Route path="/signup" element={<SignUp />}/>
-          <Route path="/login" element={<LoginButton />}/>
+          <Route path="/workout" element={<Workout />} />
+          <Route path="/signup" element={<SignUp />} />
+          <Route path="/login" element={<LoginButton />} />
         </Routes>
       </Router>
     </div>
---
+++ frontend/src/pages/user/StartWorkout.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function StartWorkout() {
+  return (
+    <div>StartWorkout</div>
+  )
+}
+
+export default StartWorkout
