06f4236a48768deb31f0d69d6bfee5e483a0f4f5
Mike790 <michael.kydd@outlook.com>
2024-07-27 13:06:38 -0400

    update schema

--- backend/models/Workout.js
+++ backend/models/Workout.js
@@ -1,64 +1,70 @@
 const mongoose = require('mongoose');
 
 const WorkoutSchema = new mongoose.Schema({
-  name: {
+  userId: {
     type: String,
-    trim: true,
-    maxlength: [50, 'name can not be more than 50 characters']
+    required: [true, 'provide userId']
   },
-  date: {
-    year: {
-      type: Number,
-      required: [true, 'provide the year']
-    },
-    month: {
-      type: Number,
-      required: [true, 'provide the month']
-    },
-    day: {
-      type: Number,
-      required: [true, 'provide the day']
-    }
-  },
-  location: {
-    coordinates: {
-      latitude: {
-        type: Number
-      },
-      longitude: {
-        type: Number
-      }
-    }
-  },
-  duration: {
-    hours: {
-      type: Number
-    },
-    minutes: {
-      type: Number
-    },
-    seconds: {
-      type: Number
-    },
-  },
-  exercises: [{
-    exerciseId: {
+  workouts: [{
+    name: {
       type: String,
-      required: [true, 'each exercise must contain an id']
+      trim: true,
+      maxlength: [50, 'name can not be more than 50 characters']
     },
-    sets: [{
-      setNumber: {
+    date: {
+      year: {
         type: Number,
-        required: [true, 'each set must contain a set number']
+        required: [true, 'provide the year']
       },
-      reps: {
+      month: {
         type: Number,
-        required: [true, 'each set must contain a reps number']
+        required: [true, 'provide the month']
       },
-      weight: {
+      day: {
         type: Number,
-        required: [true, 'each set must contain a weight number']
+        required: [true, 'provide the day']
       }
+    },
+    location: {
+      coordinates: {
+        latitude: {
+          type: Number
+        },
+        longitude: {
+          type: Number
+        }
+      }
+    },
+    duration: {
+      hours: {
+        type: Number
+      },
+      minutes: {
+        type: Number
+      },
+      seconds: {
+        type: Number
+      },
+    },
+    exercises: [{
+      exerciseId: {
+        type: String,
+        required: [true, 'each exercise must contain an id']
+      },
+      sets: [{
+        setNumber: {
+          type: Number,
+          required: [true, 'each set must contain a set number']
+        },
+        reps: {
+          type: Number,
+          required: [true, 'each set must contain a reps number']
+        },
+        weight: {
+          type: Number,
+          required: [true, 'each set must contain a weight number']
+        }
+      }]
     }]
   }]
 });
