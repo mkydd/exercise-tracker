c607122dc0126f62898895247d69cfed42863c81
Mike790 <michael.kydd@outlook.com>
2024-08-01 11:42:37 -0400

    adjust margins

--- frontend/src/pages/user/Exercises.js
+++ frontend/src/pages/user/Exercises.js
@@ -1,11 +1,12 @@
 import React from 'react'
 import { default as AllExercises } from '../../components/Workout/Exercises'
+import '../../styles/user/exercises.css'
 
 function Exercises() {
   return (
     <div className='all-exercises'>
-      Exercises
-      <AllExercises />
+      <h1>Exercises</h1>
+      <AllExercises onClickFunction={() => null}/>
     </div>
   )
 }
