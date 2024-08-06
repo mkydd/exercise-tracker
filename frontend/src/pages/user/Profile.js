56e68bfc9b8114a09aa56668844ecbee28493f32
Mike790 <michael.kydd@outlook.com>
2024-08-06 11:08:48 -0400

    add user stats

--- frontend/src/pages/user/Profile.js
+++ frontend/src/pages/user/Profile.js
@@ -5,7 +5,7 @@ import { useOutletContext } from 'react-router-dom'
 function Profile() {
   const { userWorkouts, userData } = useOutletContext()
   const [workouts, setWorkouts] = useState([])
-  const [user, setUser] = useState({})
+  const [user, setUser] = useState()
 
   useEffect(() => {
     if (userWorkouts) {
@@ -29,6 +29,20 @@ function Profile() {
         </div>
         <div className="email">
           {user && user.email}
+        </div>
+      </div>
+      <div className="stats">
+        <div className="stat total-workouts">
+          Total Workouts: <div>{workouts.length}</div>
+        </div>
+        <div className="stat age">
+          Age:&nbsp;<div>{user && user.stats.age}</div>
+        </div>
+        <div className="stat weight">
+          Weight:&nbsp;<div>{user && user.stats.weight}</div><div className='unit'>kg</div>
+        </div>
+        <div className="stat height">
+          Height:&nbsp;<div>{user && user.stats.height}</div><div className='unit'>cm</div>
         </div>
       </div>
     </div>
