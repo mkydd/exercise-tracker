199d1313f42425e0ea20435eb7e5b8d700d0c68a
Mike790 <michael.kydd@outlook.com>
2024-08-06 10:17:28 -0400

    fetch data

--- frontend/src/pages/user/Profile.js
+++ frontend/src/pages/user/Profile.js
@@ -1,8 +1,28 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import '../../styles/user/profile.css'
+import { useOutletContext } from 'react-router-dom'
 
 function Profile() {
+  const { userWorkouts, userData } = useOutletContext()
+  const [workouts, setWorkouts] = useState([])
+  const [user, setUser] = useState({})
+
+  useEffect(() => {
+    if (userWorkouts) {
+      setWorkouts(userWorkouts)
+    }
+  }, [userWorkouts])
+
+  useEffect(() => {
+    if (userData) {
+      setUser(userData)
+    }
+  }, [userData])
+
   return (
-    <div>Profile</div>
+    <div className='profile'>
+      <h1>Profile</h1>
+    </div>
   )
 }
 
