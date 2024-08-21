c23a778823ca4de5e6549bcadefaa109861f1d6e
Mike790 <michael.kydd@outlook.com>
2024-08-21 11:26:28 -0400

    style workout header

--- frontend/src/components/Workout/WorkoutHeader.js
+++ frontend/src/components/Workout/WorkoutHeader.js
@@ -1,9 +1,18 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 function WorkoutHeader() {
+  const [workoutName, setWorkoutName] = useState('Workout')
+  useEffect(() => {
+    console.log('workout name =', workoutName)
+  }, [workoutName])
   return (
     <div className='workout-header'>
-      <input type="text" name="workout-name" id="" placeholder='Workout' className='workout-header-input'/>
+      <input 
+        type="text" 
+        value={workoutName} 
+        className='workout-header-input'
+        onChange={(e) => setWorkoutName(e.target.value)}
+      />
     </div>
   )
 }
