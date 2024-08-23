d99aee7c8d29bf05f7e132f543dbbf656e3cbcb4
Mike790 <michael.kydd@outlook.com>
2024-08-23 11:53:17 -0400

    implement ConfirmationPrompt component

--- frontend/src/components/user/DeleteUserButton.js
+++ frontend/src/components/user/DeleteUserButton.js
@@ -1,9 +1,24 @@
-import React from 'react'
+import React, { useState } from 'react'
+import ConfirmationPrompt from '../ConfirmationPrompt'
 
 export default function DeleteUserButton() {
+  const [showPrompt, setShowPrompt] = useState(false)
+  const message = "Are you sure you want to delete your account. This is an irreversible action"
+
+  function deleteAccount() {
+
+  }
+  
   return (
     <div>
-      <button className="delete-user">Delete Account</button>
+      <button className="delete-user" onClick={() => setShowPrompt(true)}>Delete Account</button>
+      {showPrompt && 
+      <ConfirmationPrompt 
+        msg={message} 
+        onConfirm={() => deleteAccount()} 
+        closePrompt={() => setShowPrompt(false)}
+        display={showPrompt}/>
+      }
     </div>
   )
 }
