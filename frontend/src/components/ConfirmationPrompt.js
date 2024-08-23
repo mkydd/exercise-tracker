489d3833af60c1a7454307b92da0f99c16137ca2
Mike790 <michael.kydd@outlook.com>
2024-08-23 13:36:29 -0400

    implement ConfirmationPrompt

--- frontend/src/components/ConfirmationPrompt.js
+++ frontend/src/components/ConfirmationPrompt.js
@@ -1,27 +1,31 @@
 import React from 'react'
+import '../styles/components/confirmationPrompt.css'
 
 function ConfirmationPrompt({ msg, onConfirm, closePrompt, display }) {
 
   return (
     <div>
       {display &&
-      <div className="confirmation-prompt">
-        <div className="message">{msg}</div>
-        <div className="buttons">
-          <button 
-            className="accept" 
-            onClick={() => {
-              closePrompt(false)
-              onConfirm()
-            }}
-          >Confirm</button>
+      <div className="confirmation-prompt-wrapper">
+        <div className="confirmation-prompt">
+          <div className="message">{msg}</div>
+          <div className="buttons">
+            <button 
+              className="confirm" 
+              onClick={() => {
+                closePrompt(false)
+                onConfirm()
+              }}
+            >Confirm</button>
 
-          <button 
-            className="decline" 
-            onClick={() => closePrompt()}
-          >Cancel</button>
+            <button 
+              className="cancel" 
+              onClick={() => closePrompt()}
+            >Cancel</button>
+          </div>
         </div>
       </div>
+      
       }
     </div>
   )
