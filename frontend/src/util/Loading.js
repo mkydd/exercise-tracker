cb412b22d92cbea81fbdb55fbda5908d7c538874
Mike790 <michael.kydd@outlook.com>
2024-08-31 15:19:15 -0400

    make element position fixed

--- frontend/src/util/Loading.js
+++ frontend/src/util/Loading.js
@@ -4,9 +4,14 @@ import PulseLoader from 'react-spinners/PulseLoader'
 function Loading() {
   const styles = {
     height: '100dvh',
+    width: '100dvw',
+    position: 'fixed',
+    left: '0',
+    top: '0',
     display: 'flex',
     justifyContent: 'center',
-    alignItems: 'center'
+    alignItems: 'center',
+    backgroundColor: 'white'
   }
   
   return (
