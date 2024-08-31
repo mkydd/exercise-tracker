94cc534f7654e515dc6eaae711cb8b713ed14d54
Mike790 <michael.kydd@outlook.com>
2024-08-31 15:00:19 -0400

    add confirmation prompt

--- frontend/src/components/user/Template.js
+++ frontend/src/components/user/Template.js
@@ -1,8 +1,11 @@
 import React, { useEffect, useState } from 'react'
 import '../../styles/user/template.css'
+import ConfirmationPrompt from '../../components/ConfirmationPrompt';
 
 function Template( {workout, deleteWorkout} ) {
   const [exerciseNames, setExerciseNames] = useState([])
+  const [promptDisplay, setDisplayPrompt] = useState(false)
+  const msg = 'Are you sre you want to delete this template?'
 
   useEffect(() => {
     let temp = []
@@ -23,7 +26,7 @@ function Template( {workout, deleteWorkout} ) {
       <div className='workout-template'>
         <div className="template-header">
           <h4>{workout.name}</h4>
-          <button className='remove-template-button' onClick={deleteWorkout}>X</button>
+          <button className='remove-template-button' onClick={() => setDisplayPrompt(true)}>X</button>
         </div>
         <div className="exercises">
           {exerciseNames.join(", ")}
@@ -36,6 +39,11 @@ function Template( {workout, deleteWorkout} ) {
           {workout.date.year}
         </div>
       </div>
+      <ConfirmationPrompt 
+        display={promptDisplay}
+        msg={msg}
+        closePrompt={() => setDisplayPrompt(false)}
+        onConfirm={deleteWorkout}/>
     </div>
   )
 }
