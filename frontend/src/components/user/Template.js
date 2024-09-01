e582de574acc6e53891659a7057fe24a1d9040b0
Mike790 <michael.kydd@outlook.com>
2024-09-01 13:00:47 -0400

    remove deleteTemplate button

--- frontend/src/components/user/Template.js
+++ frontend/src/components/user/Template.js
@@ -1,13 +1,10 @@
 import React, { useEffect, useState } from 'react'
 import '../../styles/user/template.css'
-import ConfirmationPrompt from '../../components/ConfirmationPrompt';
 import { Navigate } from 'react-router-dom';
 
-function Template( {workout, deleteWorkout} ) {
+function Template( {workout} ) {
   const [exerciseNames, setExerciseNames] = useState([])
-  const [promptDisplay, setDisplayPrompt] = useState(false)
   const [displayTemplateWorkout, setDisplayTemplateWorkout] = useState(false)
-  const msg = 'Are you sre you want to delete this template?'
 
   useEffect(() => {
     let temp = []
@@ -28,7 +25,6 @@ function Template( {workout, deleteWorkout} ) {
       <div className='workout-template'>
         <div className="template-header">
           <h4>{workout.name}</h4>
-          <button className='remove-template-button' onClick={() => setDisplayPrompt(true)}>X</button>
         </div>
         <div className="exercises">
           {exerciseNames.join(", ")}
@@ -48,11 +44,6 @@ function Template( {workout, deleteWorkout} ) {
           </div>
         </div>
       </div>
-      <ConfirmationPrompt 
-        display={promptDisplay}
-        msg={msg}
-        closePrompt={() => setDisplayPrompt(false)}
-        onConfirm={deleteWorkout}/>
 
       { displayTemplateWorkout && 
         <Navigate to='/user/workout' state={{ workout }} /> }
