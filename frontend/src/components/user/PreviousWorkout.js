ec84d7d1a79c3bba813ceae3c50a6dd024e2c88d
Mike790 <michael.kydd@outlook.com>
2024-09-06 22:40:39 -0400

    remove unused import

--- frontend/src/components/user/PreviousWorkout.js
+++ frontend/src/components/user/PreviousWorkout.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState } from 'react'
 import { useOutletContext } from 'react-router-dom'
 import ConfirmationPrompt from '../ConfirmationPrompt'
 import months from '../../util/months'
