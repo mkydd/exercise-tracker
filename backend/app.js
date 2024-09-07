b22b535b6809af4a7295ebc8e5cf96646032db68
Mike790 <michael.kydd@outlook.com>
2024-09-07 11:49:13 -0400

    update cors origins

--- backend/app.js
+++ backend/app.js
@@ -13,7 +13,7 @@ const login = require('./routes/login')
 const errorHandlerMiddleware = require('./middleware/error-handling')
 
 // origins allowed to make api requests
-app.use(cors({ origin: ['https://mk-exercise-tracker.netlify.app', 'http://localhost:3000'] }));
+app.use(cors({ origin: ['https://mk-exercise-tracker.netlify.app', 'http://localhost:3000', 'https://falls-alexandria-fcc-nb.trycloudflare.com'] }));
 
 // parse application/x-www-form-urlencoded
 app.use(express.urlencoded({ extended: true }));
