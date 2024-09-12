998742b38b64b9e877ec24cb23c9f66416517fdb
Mike790 <michael.kydd@outlook.com>
2024-09-11 23:02:29 -0400

    style auth buttons

--- frontend/src/auth/login.js
+++ frontend/src/auth/login.js
@@ -4,7 +4,7 @@ import { useAuth0 } from "@auth0/auth0-react";
 const LoginButton = () => {
   const { loginWithRedirect } = useAuth0();
 
-  return <button className="auth-btn" onClick={() => loginWithRedirect()}>Log In</button>;
+  return <button className="auth-btn login" onClick={() => loginWithRedirect()}>Log In</button>;
 };
 
 export default LoginButton;
