eb00c0875c9f9e6bdf0daa110c1560e21ca0cec7
Mike790 <michael.kydd@outlook.com>
2024-09-12 15:04:50 -0400

    add verify-email route

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const router = express.Router();
 
-const { getAllUsers, createUser, getUser, updateUser, deleteUser, getSingleUser } = require('../controllers/users')
+const { getAllUsers, createUser, getUser, updateUser, deleteUser, getSingleUser, getVerificationEmail } = require('../controllers/users')
 
 router.route('/').get(getAllUsers).post(createUser)
 router.route('/:id').get(getUser).patch(updateUser).delete(deleteUser)
 router.route('/data').post(getSingleUser)
+router.route('/verify-email/:auth0Id').get(getVerificationEmail)
 
 module.exports = router;
