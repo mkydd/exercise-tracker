f7a6840e08300e35f921c2a127fb5990eb5d2122
Mike790 <michael.kydd@outlook.com>
2024-09-14 21:53:52 -0400

    display confirmation prompt on endworkout click

--- frontend/src/components/Workout/EndWorkoutButton.js
+++ frontend/src/components/Workout/EndWorkoutButton.js
@@ -1,25 +1,36 @@
 import React, { useState } from 'react'
 import Loading from '../../util/Loading';
 import { Navigate } from 'react-router-dom';
+import ConfirmationPrompt from '../ConfirmationPrompt';
 
 function EndWorkoutButton({ onFinish }) {
   const [loading, setLoading] = useState(false)
   const [goHome, setGoHome] = useState(false)
+  const [showPrompt, setShowPrompt] = useState(false)
+
+  function onEndWorkout() {
+    setLoading(true)
+    onFinish()
+    setTimeout(() => {
+      setGoHome(true)
+    }, 500);
+  }
 
   return (
     <div>
       <button 
         className="end-workout-button"
-        onClick={() => {
-          setLoading(true)
-          onFinish()
-          setTimeout(() => {
-            setGoHome(true)
-          }, 500);
-        }}>
+        onClick={() => setShowPrompt(true)}
+        >
         End Workout
       </button>
-
+      
+      <ConfirmationPrompt 
+        msg='Are you sure you want to end your workout?'
+        onConfirm={onEndWorkout}
+        closePrompt={setShowPrompt}
+        display={showPrompt}
+        buttonColor='rgb(36, 148, 36)'/> 
       { loading && <Loading /> }
       { goHome && <Navigate to='/user/home'/>}
 
