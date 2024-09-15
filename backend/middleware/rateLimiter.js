e9e2f7c1cf5f505e52799d5c61075cf8ba1e727f
Mike790 <michael.kydd@outlook.com>
2024-09-15 11:38:05 -0400

    update rate limit

--- backend/middleware/rateLimiter.js
+++ backend/middleware/rateLimiter.js
@@ -2,7 +2,7 @@ const setRateLimit = require("express-rate-limit");
 
 const rateLimiter = setRateLimit({
   windowMs: 60 * 1000,
-  max: 2,
+  max: 25,
   message: "You’ve hit the request limit. Please wait and try again in a few moments",
   headers: true,
 });
