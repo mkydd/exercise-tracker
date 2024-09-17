b67ee45a75ba5b815410c1dd6aea167d8e69edb6
Mike790 <michael.kydd@outlook.com>
2024-09-16 22:30:46 -0400

    change res

--- backend/controllers/nuke.js
+++ backend/controllers/nuke.js
@@ -4,16 +4,16 @@ const asyncWrapper = require('../middleware/async')
 const nukeMiddleware = require('../middleware/nuke')
 
 const nuke = asyncWrapper(async (req, res) => {
-  if (req.body && req.body.passcode === process.env.NUKE_PASSCODE) {
+  if (req.body.passcode === process.env.NUKE_PASSCODE) {
     const { auth0Nuked, dbNuked } = await nukeMiddleware()
 
     if (auth0Nuked && dbNuked) {
       return res.status(200).json({ msg: 'AUTH0 and DB NUKED!!!'})
     }
-    return res.status(200).json({ msg: `Auth0 nuke status: ${auth0Nuked} && DB nuke status: ${dbNuked}`})
+    return res.status(200).json({ Auth0_Status: auth0Nuked, DB_Status: dbNuked })
   }
 
-  return res.status(500).json({ msg: 'no nuke :( ' })
+  return res.status(500).json({ Auth0_Status: auth0Nuked, DB_Status: dbNuked })
 })
 
 module.exports = { nuke }
