2cd453bcc8d28dd770c519a5e4fe9540ceff4b12
Mike790 <michael.kydd@outlook.com>
2024-11-25 11:57:01 -0500

    reformat code

--- backend/app.js
+++ backend/app.js
@@ -1,26 +1,26 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 
-const bodyParser  = require('body-parser');
+const bodyParser = require("body-parser");
 app.use(bodyParser.json());
 
-const connectDB = require('./db/connect')
-const cors = require('cors');
-require('dotenv').config()
+const connectDB = require("./db/connect");
+const cors = require("cors");
+require("dotenv").config();
 
-const workouts = require('./routes/workouts')
-const users = require('./routes/users')
-const nuke = require('./routes/nuke')
-const rateLimiter = require('./middleware/rateLimiter')
+const workouts = require("./routes/workouts");
+const users = require("./routes/users");
+const nuke = require("./routes/nuke");
+const rateLimiter = require("./middleware/rateLimiter");
 
-const errorHandlerMiddleware = require('./middleware/error-handling')
-const checkJWT = require('./middleware/checkJWT')
+const errorHandlerMiddleware = require("./middleware/error-handling");
+const checkJWT = require("./middleware/checkJWT");
 
 // origins allowed to make api requests
-app.use(cors({ origin: ['https://mk-exercise-tracker.netlify.app'] }));
+app.use(cors({ origin: ["https://mk-exercise-tracker.netlify.app"] }));
 
 // limit request
-app.use(rateLimiter)
+app.use(rateLimiter);
 
 // parse application/x-www-form-urlencoded
 app.use(express.urlencoded({ extended: true }));
@@ -29,25 +29,25 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // non-token routes
-app.use('/api/v1/nuke', nuke)
+app.use("/api/v1/nuke", nuke);
 
 // authorize user
-app.use(checkJWT)
+app.use(checkJWT);
 
 // routes
-app.use('/api/v1/users/workouts', workouts)
-app.use('/api/v1/users', users)
-app.use(errorHandlerMiddleware)
+app.use("/api/v1/users/workouts", workouts);
+app.use("/api/v1/users", users);
+app.use(errorHandlerMiddleware);
 
 const port = process.env.PORT || 5001;
 
 const start = async () => {
   try {
-    await connectDB(process.env.MONGO_URI)
-    app.listen(port, console.log(`Server is listening on port ${port}`))
+    await connectDB(process.env.MONGO_URI);
+    app.listen(port, console.log(`Server is listening on port ${port}`));
   } catch (error) {
-    console.log(error)
+    console.log(error);
   }
-}
+};
 
-start()
+start();
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,26 +1,25 @@
 import Workout from "./components/Workout/Workout";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import User from "./pages/user/User";
-import './styles/largeStyles.css'
-import './styles/app.css'
+import "./styles/app.css";
+import "./styles/largeStyles.css";
 
 import PrivateRoute from "./auth/ProtectedRoute";
 
 // user Routes
 import Exercises from "./pages/user/Exercises";
-import History from './pages/user/History';
-import Profile from './pages/user/Profile';
+import History from "./pages/user/History";
+import Profile from "./pages/user/Profile";
 import UserHome from "./pages/user/StartNewWorkout";
 import Home from "./pages/home";
 
 function App() {
-  
   return (
     <div className="app">
       <Router>
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route element={<PrivateRoute/>}>
+          <Route element={<PrivateRoute />}>
             <Route path="user" element={<User />}>
               <Route path="home" element={<UserHome />} />
               <Route path="profile" element={<Profile />} />
